19ed362cff59c05f6491e94d0ce70e9a8e316ea2
Ryan Harris <ryanbuiltthat@users.noreply.github.com>
2025-11-21 06:49:10 -0500

    Update CONFIG_SCHEMA to use _SENSOR_SCHEMA

--- components/zmpt101b/sensor/__init__.py
+++ components/zmpt101b/sensor/__init__.py
@@ -7,7 +7,8 @@ sensor_ns = cg.esphome_ns.namespace('zmpt101b_ns')
 
 Zmpt101bSensor = sensor_ns.class_('ZMPT101BSensor', cg.PollingComponent)
 
-CONFIG_SCHEMA = sensor.SENSOR_SCHEMA.extend({
+#CONFIG_SCHEMA = sensor.SENSOR_SCHEMA.extend({
+CONFIG_SCHEMA = sensor._SENSOR_SCHEMA.extend({    
     cv.GenerateID(): cv.declare_id(Zmpt101bSensor),
     cv.Required("adc_pin"): cv.int_range(min=0, max=99),
     cv.Optional("sensitivity", default=500): cv.float_range(min=0, max=1000)
